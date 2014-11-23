f4dde74a217ca55e4627283e4d49c2f5de3b631e
Kim <kimxx27@hotmail.com>
2014-11-23 23:07:35 +0100

    Startup

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mover : MonoBehaviour {
+	
+	private Vector3 Direction;
+	private Vector3 MoveVector;
+	public float MoveSpeed;
+	public List<Transform> waypoints = new List<Transform>();
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Direction = way.transform.position- transform.position;
+		//MoveVector = Direction.normalized * MoveSpeed * Time.deltaTime;
+		//transform.position += MoveVector;
+	}
+}
