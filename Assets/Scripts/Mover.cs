cd7bd4a4676a94ea6aa80fd6df8fc068f8404db5
Kim <kimxx27@hotmail.com>
2014-11-23 23:27:49 +0100

    waypoints

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,8 +7,11 @@ public class Mover : MonoBehaviour {
 	private Vector3 Direction;
 	private Vector3 MoveVector;
 	public float MoveSpeed;
+	public float MinDistance;
 	public List<Transform> waypoints = new List<Transform>();
 
+	private Transform CurrentWaypoint;
+	private int CurrentIndex;
 
 
 
@@ -16,14 +19,28 @@ public class Mover : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		CurrentWaypoint = waypoints[0];
+		CurrentIndex = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		//Direction = way.transform.position- transform.position;
-		//MoveVector = Direction.normalized * MoveSpeed * Time.deltaTime;
-		//transform.position += MoveVector;
-	}
+				Direction  = CurrentWaypoint.transform.position - transform.position;
+				MoveVector = Direction.normalized * MoveSpeed * Time.deltaTime;
+				transform.position += MoveVector;
+				transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (Direction), 4 * Time.deltaTime);
+
+		
+				if (Vector3.Distance (CurrentWaypoint.transform.position, transform.position) < MinDistance) {
+						++CurrentIndex;
+						if (CurrentIndex > waypoints.Count - 1) {
+								CurrentIndex = 0;
+						}
+						CurrentWaypoint = waypoints [CurrentIndex];
+
+				}
+		}
+
+
 }
