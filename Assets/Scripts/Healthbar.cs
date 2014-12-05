e5ffc7431e7a3798d7606f69af17b65a5ac6ccdd
Aleci <barbeos@gmail.com>
2014-12-05 14:32:51 +0100

    Targeting for tower and Health script

---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Healthbar : MonoBehaviour {
+
+	public int maxHealth = 100;
+	public int curHealth = 100;
+	
+	public float healthBarLength;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AddjustCurrentHealth(0);   
+	}
+	
+	void OnGUI()
+	{
+		
+		Vector2 targetPos;
+		targetPos = Camera.main.WorldToScreenPoint (transform.position);
+		
+		GUI.Box(new Rect(targetPos.x - 20, Screen.height- targetPos.y - 20, healthBarLength, 5), curHealth + "/" + maxHealth);
+		
+	}
+	
+	public void AddjustCurrentHealth(int adj) {
+		curHealth += adj;
+		
+		if (curHealth < 0)
+			curHealth = 0;
+		
+		if (curHealth > maxHealth)
+			curHealth = maxHealth;
+		
+		if(maxHealth < 1)
+			maxHealth = 1;
+		
+		healthBarLength = (Screen.width / 25) * (maxHealth / curHealth);
+	}
+
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tower : MonoBehaviour {
+	
+	public float fireRate = 0.5f;
+	public int Damage;
+	private List<GameObject> mobList = new List<GameObject>();   
+	
+	private bool isFiring = false;
+	
+	
+	private bool fireAtGameObject(GameObject target){
+		bool destroyGameObject = false;
+		
+		Healthbar.
+
+		destroyGameObject = true;
+		
+		
+		return destroyGameObject;
+	}
+	
+	
+	private void OnTriggerEnter(Collider other){
+		
+		GameObject theObject = other.attachedRigidbody.gameObject; 
+		
+		if(!mobList.Contains(theObject)){
+			mobList.Add(theObject);
+			if(isFiring == false){ 
+				isFiring = true;
+				Invoke("doFireSequence",fireRate);
+			}
+		}
+	}
+	
+	private void OnTriggerExit(Collider other){
+		GameObject theObject = other.attachedRigidbody.gameObject; 
+		
+		if(mobList.Contains(theObject)){ 
+			mobList.Remove(theObject); 
+		}
+	}
+	
+	private void doFireSequence(){
+		if(mobList.Count > 0){
+			if( fireAtGameObject( mobList[0] )){
+				
+				Destroy (mobList[0]);
+				mobList.RemoveAt(0);
+			}
+			Invoke("doFireSequence",fireRate);
+		} else {
+			isFiring = false;
+		}
+	}
+	
+}
