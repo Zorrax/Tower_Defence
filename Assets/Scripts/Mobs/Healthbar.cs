b59e91ee418900d2d3ac00eb859d3d852def4aad
Kim <kimxx27@hotmail.com>
2014-12-31 17:01:06 +0100

    infinite armor bug

--- Assets/Scripts/Mobs/Healthbar.cs
+++ Assets/Scripts/Mobs/Healthbar.cs
@@ -2,54 +2,62 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class Healthbar : MonoBehaviour {
+public class Healthbar : MonoBehaviour
+{
 
-	public float StartHealth;
-	public MobType me;
+    public float StartHealth;
+    public MobType me;
+    
+    public float healthBarLength;
 
-	public float healthBarLength;
-	
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
-	}
-	public void SetType(MobType type){
-		me = new MobType (type);
-		StartHealth = me.Health;
-		healthBarLength =  (me.Health / StartHealth);
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
-	
-	public void AddjustCurrentHealth(DamageClass Damage) {
+    }
+    public void SetType(MobType type)
+    {
+        me = new MobType(type);
+        StartHealth = me.Health;
+        healthBarLength = (me.Health / StartHealth);
+    }
 
-        float fireDamage = Damage.Fire*(1f-me.FireResistance/100f);
+
+    public void AddjustCurrentHealth(DamageClass Damage)
+    {
+        float fireRatio=0, physicalRatio=0;
+        float fireDamage = Damage.Fire * (1f - me.FireResistance / 100f);
         float physicalDamage = Damage.Physical * (1f - me.PhysicalResistance / 100f);
 
-        float fireRatio = fireDamage / me.Health;
-        float physicalRatio = physicalDamage / me.Health;
+        if (me.Health > 0)
+        {
+            fireRatio = fireDamage / me.Health;
+            physicalRatio = physicalDamage / me.Health;
+        }
 
 
         me.Health -= fireDamage;
         me.Health -= physicalDamage;
 
-		me.FireResistance -= Damage.FirePen-fireRatio*10;
-		me.PhysicalResistance -= Damage.PhysicalPen-physicalRatio*10;
-		
-		if (me.Health < 0)
-			me.Health = 0;
-		
-		if (me.Health > StartHealth)
-			me.Health = StartHealth;
-		
-		if(StartHealth < 1)
-			StartHealth = 1;
-		
-		healthBarLength = (me.Health / StartHealth);
-	}
+        me.FireResistance -= Damage.FirePen - fireRatio * 10;
+        me.PhysicalResistance -= Damage.PhysicalPen - physicalRatio * 10;
+
+        if (me.Health < 0)
+            me.Health = 0;
+
+        if (me.Health > StartHealth)
+            me.Health = StartHealth;
+
+        if (StartHealth < 1)
+            StartHealth = 1;
+
+        healthBarLength = (me.Health / StartHealth);
+    }
 
 }
